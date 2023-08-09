0e77bf090ab1c900402a5d43df329fc105b3fe27
Vadim <seigen@mail.ru>
2023-08-09 12:49:08 +0500

    start project

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// let a = 5;
-// const LeftBolderWidth = 1;
-// a= 10;
-// console.log(a);
-// console.log(-6/0);
-let incr =10;
-    decr =10;
+const numbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-incr++;
-decr--;
 
-console.log(incr);
+const personalMovieDB = {
+       count: numbersOfFilms,
+       movies: {},
+       actors:{},
+       genres:[],
+       privat: false
+
+};
+
+const a = prompt('Один из просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c = prompt('Один из просмотренных фильмов?', ''),
+       d = prompt('На сколько оцените его?', '');
+
+       personalMovieDB.movies[a] = b;
+       personalMovieDB.movies[c] = d;
+
+       console.log(personalMovieDB);
